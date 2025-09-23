39e5e820b3fd2da27b1dddf9be8f5d8235496ad8
Jose Ramirez <99100085+joseramirez1805@users.noreply.github.com>
2025-09-23 16:32:36 -0500

    Challenge-07

---
+++ challenge-07/src/components/mockPages.jsx
@@ -0,0 +1,5 @@
+export const pages = [
+  { id: 1, name: "Home", path: "/" },
+  { id: 2, name: "Simple List", path: "/simple" },
+  { id: 3, name: "Double List", path: "/double" }
+];
---
+++ challenge-07/src/pages/Home.jsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <h1>Este es el challenge - 07</h1>;
+}
